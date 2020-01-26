652eaa59d1731196d4538604dc070f21c023be30
Adrian Orcik <adrian.orcik@gmail.com>
2020-01-26 09:28:49 +0200

    Implement game core loop

--- Lua_Embedding/Lua_Embedding/src/LuaScripts/LuaScript.lua
+++ Lua_Embedding/Lua_Embedding/src/LuaScripts/LuaScript.lua
@@ -11,14 +11,6 @@
 -- sprite:LoadSprite("src/Sprites/Tank_1.txt",3)
 -- sprite:Draw(10,15)
 
--- AnimatorController = Animator.Create()
--- AnimatorController:AddToSheet("src/Sprites/Tank_Shot_1.txt",3)
--- AnimatorController:AddToSheet("src/Sprites/Tank_Shot_2.txt",3)
--- AnimatorController:AddToSheet("src/Sprites/Tank_Shot_3.txt",3)
-
--- while true do
---     AnimatorController:Animate(20,15,100)
--- end
 
 
 --Test
@@ -53,64 +45,96 @@
 --print(string.format("input: %.1f",input))
 --print(input)
 
+--Animations
+PlayerAnimation = Animator.Create()
+PlayerAnimation:AddToSheet("src/Sprites/Tank_Shot_1.txt",3)
+PlayerAnimation:AddToSheet("src/Sprites/Tank_Shot_2.txt",3)
+PlayerAnimation:AddToSheet("src/Sprites/Tank_Shot_3.txt",3)
 
--- --Functions
--- UICanvas    = Canvas.Create()
+TankAnimation = Animator.Create()
+TankAnimation:AddToSheet("src/Sprites/Explosion_1.txt",4)
+TankAnimation:AddToSheet("src/Sprites/Explosion_2.txt",4)
+TankAnimation:AddToSheet("src/Sprites/Explosion_3.txt",4)
 
--- Player      = Sprite.Create()
--- Player:LoadSprite("src/Sprites/Tank_1.txt",3)
+--Functions
+UICanvas    = Canvas.Create()
 
--- EnemyObj    = GameObject.Create()
--- Enemy       = Sprite.Create()
--- Enemy:LoadSprite("src/Sprites/Tank_2.txt",3)
--- EnemyHealth = 2
+Player      = Sprite.Create()
+Player:LoadSprite("src/Sprites/Tank_1.txt",3)
+IsPlayerShoot = false
 
--- function IsEnemyInDistance(_distance, _enemyPos)
---         local min = _distance - 5
---         local max = _distance + 5
---         if((_enemyPos >= min) and (_enemyPos <= max))
---             then return true end
---     do return false end
--- end
+EnemyObj    = GameObject.Create()
+EnemySprite       = Sprite.Create()
+EnemySprite:LoadSprite("src/Sprites/Tank_2.txt",3)
+EnemyHealth = 2
+EnemyAlive = true
 
--- function EnemySetPosition(_x,_y)
---     EnemyObj.x = _x
---     EnemyObj.y = _y
--- end
+function IsEnemyInDistance(_distance, _enemyPos)
+        local min = _distance - 5
+        local max = _distance + 5
+        if((_enemyPos >= min) and (_enemyPos <= max))
+            then return true end
+    do return false end
+end
 
+function EnemySetPosition(_x,_y)
+    EnemyObj.x = _x
+    EnemyObj.y = _y
+end
 
--- function Game()
---     UICanvas:Text(0,0,"Tank Shot 2D - Embedded Lua to CPP")
+function Enemy()
+    EnemySetPosition(80,10)
+    if EnemyHealth > 0 then 
+        UICanvas:Text(EnemyObj.x + 2,EnemyObj.y - 1,"HP: " .. tostring(EnemyHealth))
+        EnemySprite:Draw(EnemyObj.x,EnemyObj.y)
+    else
+        if EnemyAlive == true and IsPlayerShoot == false then
+            TankAnimation:Animate(EnemyObj.x,EnemyObj.y,100)
+            EnemyAlive = false
+            Game()
+        end
+    end
+end
+
+function Game()
+    UICanvas:Text(0,0,"Tank Shot 2D - Embedded Lua to CPP")
     
---     EnemySetPosition(80,10)
---     if EnemyHealth > 0 then 
---         UICanvas:Text(EnemyObj.x + 2,EnemyObj.y - 1,"HP: " .. tostring(EnemyHealth))
---         Enemy:Draw(EnemyObj.x,EnemyObj.y)
---     end
+    Enemy()
     
---     UICanvas:Text(81,12,".")
+    UICanvas:Text(81,12,".")
     
---     Player:Draw(5,10)
--- end
+    Player:Draw(5,10)
 
--- function PlayerInput()
---     local playerAngle = UICanvas:InputField_Int(0,20,"Angle: ")
---     local playerSpeed = UICanvas:InputField_Int(0,21,"Speed: ")
---     local distance = Math.MaxDistance(playerSpeed,playerAngle)
---     print(distance)
+    if IsPlayerShoot == true then
+        PlayerAnimation:Animate(5,10,100)
+        IsPlayerShoot = false
+        Game()
+    end
 
---     local result = IsEnemyInDistance(distance,EnemyObj.x)
---     if result == true then
---         EnemyHealth = EnemyHealth - 1
---     end
+end
 
---     print(result)
--- end
+function PlayerInput()
+    local playerAngle = UICanvas:InputField_Int(0,20,"Angle: ")
+    local playerSpeed = UICanvas:InputField_Int(0,21,"Speed: ")
+    local distance = Math.MaxDistance(playerSpeed,playerAngle)
+    print(distance)
 
--- --Main core loop
--- while true do
---     Game()
---     PlayerInput()
---     Console.Sleep(1)
---     Console.Clear()
--- end
+    local result = IsEnemyInDistance(distance,EnemyObj.x)
+    if result == true then
+        EnemyHealth = EnemyHealth - 1
+        IsPlayerShoot = true
+    end
+
+    print(result)
+end
+
+--Main core loop
+while true do
+    Game() --Draw idle scene 
+    if IsPlayerShoot == false then
+        PlayerInput()
+    end
+    Game() --Draw scene with animations
+    Console.Sleep(1)
+    Console.Clear()
+end
