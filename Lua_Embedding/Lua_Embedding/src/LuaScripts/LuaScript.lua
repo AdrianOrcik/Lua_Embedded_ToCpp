0c4e80ee070c70881a9fc2a3e5b9ff9e911673d3
Adrian Orcik <adrian.orcik@gmail.com>
2020-01-26 10:00:27 +0200

    Refactor Lua scripts

--- Lua_Embedding/Lua_Embedding/src/LuaScripts/LuaScript.lua
+++ Lua_Embedding/Lua_Embedding/src/LuaScripts/LuaScript.lua
@@ -1,49 +1,6 @@
-
---player = GameObject.Create()
---
---player:Move(5,7)
---player:Draw()
---
---player:Move(5,10)
---player:Draw()
-
--- sprite = Sprite.Create()
--- sprite:LoadSprite("src/Sprites/Tank_1.txt",3)
--- sprite:Draw(10,15)
-
-
-
---Test
---
--- Tank1 = Sprite.Create()
--- Tank1:LoadSprite("src/Sprites/Tank_1.txt",3)
--- Tank1:Draw(10,15)
---
---Tank2 = Sprite.Create()
---Tank2:LoadSprite("src/Sprites/Tank2.txt",3)
---Tank2:Draw(50,15)
---
-
---angle = Math.DegToRad(45)
---print(angle)
---
---Console.Sleep(2)
---Console.Clear()
-
--- player = GameObject.Create()
--- player:Move(5,7)
--- player:Draw()
--- print(player.x)
--- player.x = 10
--- print(player.x)
-
---canvas = Canvas.Create()
---mainText = "Value: " .. tostring(5)
---print(mainText)
---canvas:Text(10,15,"Value: " .. tostring(10))
---input = canvas:InputField_Float(10,16,"Test input:")
---print(string.format("input: %.1f",input))
---print(input)
+--Projectile particles
+ProjectileArr = {}
+ProjectileIndex = 1;
 
 --Animations
 PlayerAnimation = Animator.Create()
@@ -56,20 +13,24 @@ TankAnimation:AddToSheet("src/Sprites/Explosion_1.txt",4)
 TankAnimation:AddToSheet("src/Sprites/Explosion_2.txt",4)
 TankAnimation:AddToSheet("src/Sprites/Explosion_3.txt",4)
 
---Functions
+--UI Canvas
 UICanvas    = Canvas.Create()
 
+--Player properties
 Player      = Sprite.Create()
 Player:LoadSprite("src/Sprites/Tank_1.txt",3)
 IsPlayerShoot = false
 
+--Enemy properties
 EnemyObj    = GameObject.Create()
 EnemySprite       = Sprite.Create()
 EnemySprite:LoadSprite("src/Sprites/Tank_2.txt",3)
 EnemyHealth = 2
 EnemyAlive = true
 
+--Functions
 function IsEnemyInDistance(_distance, _enemyPos)
+    --Calcualtion if player hit enemy tank
         local min = _distance - 5
         local max = _distance + 5
         if((_enemyPos >= min) and (_enemyPos <= max))
@@ -84,6 +45,8 @@ end
 
 function Enemy()
     EnemySetPosition(80,10)
+
+    --Enemy behavior of tank render & tank animation
     if EnemyHealth > 0 then 
         UICanvas:Text(EnemyObj.x + 2,EnemyObj.y - 1,"HP: " .. tostring(EnemyHealth))
         EnemySprite:Draw(EnemyObj.x,EnemyObj.y)
@@ -101,40 +64,53 @@ function Game()
     
     Enemy()
     
-    UICanvas:Text(81,12,".")
-    
+    --Projectile particles draw 
+    for i = 1 , ProjectileIndex do
+        if(ProjectileArr[i] ~= nill) then
+            UICanvas:Text(ProjectileArr[i],12,".")
+        end
+    end
+
     Player:Draw(5,10)
 
+    --If player is currently in shoot state play animation
     if IsPlayerShoot == true then
         PlayerAnimation:Animate(5,10,100)
         IsPlayerShoot = false
         Game()
     end
-
 end
 
 function PlayerInput()
+    --Player projectile input
     local playerAngle = UICanvas:InputField_Int(0,20,"Angle: ")
     local playerSpeed = UICanvas:InputField_Int(0,21,"Speed: ")
     local distance = Math.MaxDistance(playerSpeed,playerAngle)
     print(distance)
 
+    --Check if player hit enemy
     local result = IsEnemyInDistance(distance,EnemyObj.x)
     if result == true then
         EnemyHealth = EnemyHealth - 1
-        IsPlayerShoot = true
     end
+
+    --Settings for animation & projectile particles
+    IsPlayerShoot = true
+    ProjectileArr[ProjectileIndex] = distance
+    ProjectileIndex = ProjectileIndex + 1
 
     print(result)
 end
 
 --Main core loop
 while true do
-    Game() --Draw idle scene 
+    --Draw idle scene 
+    Game() 
     if IsPlayerShoot == false then
         PlayerInput()
     end
-    Game() --Draw scene with animations
+    --Draw scene with animations
+    Game() 
     Console.Sleep(1)
     Console.Clear()
 end
---
+++ Lua_Embedding/Lua_Embedding/src/LuaScripts/LuaTest.lua
@@ -0,0 +1,46 @@
+
+--player = GameObject.Create()
+--
+--player:Move(5,7)
+--player:Draw()
+--
+--player:Move(5,10)
+--player:Draw()
+
+-- sprite = Sprite.Create()
+-- sprite:LoadSprite("src/Sprites/Tank_1.txt",3)
+-- sprite:Draw(10,15)
+
+
+
+--Test
+--
+-- Tank1 = Sprite.Create()
+-- Tank1:LoadSprite("src/Sprites/Tank_1.txt",3)
+-- Tank1:Draw(10,15)
+--
+--Tank2 = Sprite.Create()
+--Tank2:LoadSprite("src/Sprites/Tank2.txt",3)
+--Tank2:Draw(50,15)
+--
+
+--angle = Math.DegToRad(45)
+--print(angle)
+--
+--Console.Sleep(2)
+--Console.Clear()
+
+-- player = GameObject.Create()
+-- player:Move(5,7)
+-- player:Draw()
+-- print(player.x)
+-- player.x = 10
+-- print(player.x)
+
+--canvas = Canvas.Create()
+--mainText = "Value: " .. tostring(5)
+--print(mainText)
+--canvas:Text(10,15,"Value: " .. tostring(10))
+--input = canvas:InputField_Float(10,16,"Test input:")
+--print(string.format("input: %.1f",input))
+--print(input)
